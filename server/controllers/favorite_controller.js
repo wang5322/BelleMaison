3c8adca90f616943dbc06de0207591b134861176
JingWeiCandy <2343458@johnabbottcollege.net>
2023-10-08 15:41:25 -0400

    tested from postman

--- server/controllers/favorite_controller.js
+++ server/controllers/favorite_controller.js
@@ -6,17 +6,26 @@ const { validateToken } = require("../middlewares/AuthMiddleware");
 
 module.exports = {
     setFavorite: [validateToken, async (req, res) => {
-        const {property_id} = req.body;
-        const user_id = req.user.id;
+        const {property_id, user_id} = req.body;
+        //const user_id = req.user.id;
         const found = await Favorites.findOne({
             where: { property_id: property_id, user_id: user_id },
+        }).catch((err)=>{
+            res.json(err);
         });
 
         if(!found){
-            await Favorites.create({property_id: property_id, user_id: user_id});
+            await Favorites.create({property_id: property_id, user_id: user_id})
+            .catch((err)=>{
+                return res.json(err);
+            });
             res.json({liked: true});
         }else {
-            await Favorites.destroy({where: { property_id: property_id, user_id: user_id }});
+            await Favorites.destroy({where: 
+                { property_id: property_id, user_id: user_id }})
+            .catch((err)=>{
+                return res.json(err);
+            });
             res.json({liked: false});
         }
     }]
