58ba58ca2e02ad1b3624b871e86fc8252c795bfa
JingWeiCandy <2343458@johnabbottcollege.net>
2023-10-08 17:20:40 -0400

    add navbar and search compoments

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; // Switch is replaced by Routes
 import TestPicture from "./pages/TestPicture";
+import Navbar from './components/HomeNavbar';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <SearchBar />
       <Router>
         {/* Routes section */}
         <Routes>
