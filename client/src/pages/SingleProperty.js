4e323a139dc2b0d6ba89d072340071b66ff34ac4
wang5322 <124798791+wang5322@users.noreply.github.com>
2023-10-10 11:45:14 -0400

    created moartage calculator

--- client/src/pages/SingleProperty.js
+++ client/src/pages/SingleProperty.js
@@ -16,10 +16,16 @@ import {
 } from "react-geocode";
 import "../App.css";
 import Container from 'react-bootstrap/Container';
+// import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Calculator from "../components/Calculator";
 
 const SingleProperty = () => {
+  //mortgage calculator
+  const [value, setValue] = React.useState(50);
+  //map
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
@@ -37,55 +43,78 @@ const SingleProperty = () => {
       console.log(property);
       const tempAddress = `${res.data.address},${res.data.city}`;
       return fromAddress(tempAddress);
-    }).then((res)=>{
-        setCenterState(res.results[0].geometry.location);
-        return fromAddress("4330 Sherbrooke St W, Westmount, Quebec H3Z 1E2");
-      }).then((res) => {
-        console.log(res);
-        setDestinationState(res.results[0].geometry.location);
-      }).catch((err) => {
-        console.log(err);
-      }); 
+    }).then((res) => {
+      setCenterState(res.results[0].geometry.location);
+      return fromAddress("4330 Sherbrooke St W, Westmount, Quebec H3Z 1E2");
+    }).then((res) => {
+      console.log(res);
+      setDestinationState(res.results[0].geometry.location);
+    }).catch((err) => {
+      console.log(err);
+    });
   }, []);
 
   return (
-    <Container fluid='md'>
+    <div class="APP">
+      <Container >
+        <Row>
+          <Col md={6}>
+            <div className="calculator" style={{ maxWidth: 500, margin: "1rem auto" }} >
+              <h2>Mortgage Calculator</h2>
+              <Calculator />
+            </div>
+          </Col>
+          <Col md={6}>
+            <h2>result</h2>
+          </Col>
+        </Row>
 
+      </Container>
+      <Container>
+        <div class="row">
+          <div class="col-md-6">
+            <h2>View on Map</h2>
+            <p>distance to John Abott College</p>
+          </div>
+        </div>
 
-      <h2>Mortgage Calculator</h2>
+        <div class="col-md-6">
+          <div className="SingleProperty">
+            {!isLoaded ? (
+              <h1>Loading...</h1>
+            ) : (
+              <GoogleMap
+                mapContainerClassName="map-container"
+                center={center}
+                zoom={10}
+              >
+                <MarkerF
+                  position={center}
+                  icon="http://maps.google.com/mapfiles/kml/pal2/icon10.png" />
+                <MarkerF
+                  position={destination}
+                  icon="http://maps.google.com/mapfiles/kml/pal2/icon10.png" />
+                <DistanceMatrixService
+                  options={{
+                    origins: [center],
+                    destinations: [destination],
+                    travelMode: "DRIVING",
+                  }}
+                  callback={(response) => {
+                    const distance = response.rows[0].elements[0].distance;
+                    if (distance) {
+                      console.log(response.rows[0].elements[0].distance.text);
+                    }
+                  }}
+                />
+              </GoogleMap>
+            )}
+          </div>
 
-      <div className="SingleProperty">
-        {!isLoaded ? (
-          <h1>Loading...</h1>
-        ) : (
-          <GoogleMap
-            mapContainerClassName="map-container"
-            center={center}
-            zoom={10}
-          >
-            <MarkerF
-              position={center}
-              icon="http://maps.google.com/mapfiles/kml/pal2/icon10.png" />
-            <MarkerF
-              position={destination}
-              icon="http://maps.google.com/mapfiles/kml/pal2/icon10.png" />
-            <DistanceMatrixService
-              options={{
-                origins: [center],
-                destinations: [destination],
-                travelMode: "DRIVING",
-              }}
-              callback={(response) => {
-                const distance = response.rows[0].elements[0].distance;
-                if (distance) {
-                  console.log(response.rows[0].elements[0].distance.text);
-                }
-              }}
-            />
-          </GoogleMap>
-        )}
-      </div>
-    </Container>
+        </div>
+
+      </Container>
+    </div>
   );
 };
 
